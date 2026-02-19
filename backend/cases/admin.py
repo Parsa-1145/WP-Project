657f1a124bcf8046bb65cd42808d1cb7c1128e72
Parsa P. Behrouz <parsaymm@gmail.com>
2026-02-19 21:18:58 +0330

    Backend: register objects for admin site

--- backend/cases/admin.py
+++ backend/cases/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Case
+from .models import *
 
 # Register your models here.
 admin.site.register(Case)
+admin.site.register(Complaint)
+admin.site.register(CrimeScene)
