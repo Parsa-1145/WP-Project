c396ae7c4b4d43886eca1f47443350381f78eaea
AliAlmasiZ <ali1385almasi@gmail.com>
2026-02-22 21:23:42 +0330

    Backend: CustomUserAdmin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("username", "email")
+    ordering = ("-date_joined",)
+    readonly_fields = ("last_login", "date_joined")
+    fieldsets = (
+        (None,
+          {"fields": ("username", "email", "password")}
+        ),
+        ("Permissions",
+          {"fields": ("is_staff", "is_active", "groups", "user_permissions")}
+        ),
+        ("Dates", 
+         {"fields": ("last_login", "date_joined")}
+        ),
+    )
+
