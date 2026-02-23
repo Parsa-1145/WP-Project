521698d5d4c27249baa8f5f451469df73f8deda7
AliAlmasiZ <ali1385almasi@gmail.com>
2026-02-24 01:24:59 +0330

    Backend: (payments) fixed admin panel for transactions

--- backend/payments/admin.py
+++ backend/payments/admin.py
@@ -7,6 +7,9 @@ class PaymentTransactionAdmin(admin.ModelAdmin):
     list_filter = ("status", "created_at")
     search_fields = ("user__username", "ref_id")
     ordering = ("-created_at",)
+    readonly_fields = ("authority", "gateway_message", "ref_id", "created_at", "updated_at", "completed_at", "bail_request", "amount", "user")
+    def has_add_permission(self, request):
+        return False
 
 @admin.register(BailRequest)
 class BailRequestAdmin(admin.ModelAdmin):
